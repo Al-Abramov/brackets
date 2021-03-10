834db860a35a8af2e87c5045beecc5b5cbd3af15
Alex <x3m.f@mail.ru>
2021-03-10 21:25:25 +0400

    feat: add function for tasks Brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let newConfig = bracketsConfig.map(i => i.join(''));
+
+    for(let i = 0; i < newConfig.length; i++ ) {
+       if(str.includes(newConfig[i])) {
+           str = str.replace(newConfig[i], '');
+           i = -1;
+       }
+    }
+    return (!str) ? true : false;
 }
